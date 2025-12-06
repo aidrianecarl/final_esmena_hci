c10867da6becf2314154f92f2c6465228eab4fab
Aidriane Carl Esmena <aidrianecarlesmena@gmail.com>
2025-12-06 09:06:49 +0800

    Update Login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ include 'api/auth/login.php'
     <title>Login - <?php echo APP_NAME; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="asstes/css/login.css">
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
     <div class="login-container">
